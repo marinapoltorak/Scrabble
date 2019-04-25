7ac0070e53e608729a03b1a4e55f81149486663b
Marina Poltorak and Zsuzsanna Mangu <marina@>
2019-04-25 16:21:04 -0700

    add functionality for whole words

--- Scrabble.Tests/ModelTests/WordTests.cs
+++ Scrabble.Tests/ModelTests/WordTests.cs
@@ -20,13 +20,26 @@ namespace Scrabble.Tests
     public void ValueToMatch_WhatIsTheValueOfALetter_Number()
     {
       //Arrange
-      Word newWord = new Word("a");
+      Word newWord = new Word("b");
 
       //Act
-      int result = newWord.ValueToMatch("a");
+      int result = newWord.ValueToMatch("b");
 
       //Assert
-      Assert.AreEqual(1, result);
+      Assert.AreEqual(2, result);
+    }
+      [TestMethod]
+
+      public void ValueToMatch_WhatIsTheValueOfALetterS_Number()
+      {
+        //Arrange
+        Word newWord = new Word("apple");
+
+        //Act
+        int result = newWord.ValueToMatch("apple");
+
+        //Assert
+        Assert.AreEqual(8, result);
     }
   }
 }
--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -5,10 +5,9 @@ namespace Scrabble.Models
 {
   public class Word
   {
-
     private string _word;
 
-    public Dictionary<string, int> _scrabbleDictionary = new Dictionary<string, int>() { {"a", 1} };
+    public Dictionary<char, int> _scrabbleDictionary = new Dictionary<char, int>() { {'a', 1}, {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1}, {'l', 1}, {'n', 1}, {'r', 1}, {'s', 1}, {'t', 1}, {'d', 2}, {'g', 2}, {'b', 3}, {'c', 3}, {'m', 3}, {'p', 3}, {'f', 4}, {'h', 4}, {'v', 4}, {'w', 4}, {'y', 4}, {'k', 5}, {'j', 58}, {'x', 8}, {'q', 10}, {'z', 10} };
 
     public Word (string word)
     {
@@ -25,23 +24,24 @@ namespace Scrabble.Models
       //whatever is at the first place: what are we looking for? what do we want to do something to?
       //whatever is at the second place: where is that first thing located at?
       {
-        if (letter == 'a')
+        if (_scrabbleDictionary.ContainsKey(letter))
         {
-          total++;
-        }
-        else
-        {
-          total = 0;
+          total = total + _scrabbleDictionary[letter];
         }
       }
-        return total;
-        // total = total + _scrabbleDictionary['a'];
-
-
+      return total;
     }
   }
 }
 
 
-//
-// , {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1}, {'l', 1}, {'n', 1}, {'r', 1}, {'s', 1}, {'t', 1}, {'d', 2}, {'g', 2}, {'b', 3}, {'c', 3}, {'m', 3}, {'p', 3}, {'f', 4}, {'h', 4}, {'v', 4}, {'w', 4}, {'y', 4}, {'k', 5}, {'j', 58}, {'x', 8}, {'q', 10}, {'z', 10}
+
+
+// for (int index=0; index < word.Length; index++)
+// {
+//   if (_scrabbleDictionary.ContainsKey(word))
+//   {
+//     total = total + _scrabbleDictionary[word];
+//   }
+// }
+// return total;
